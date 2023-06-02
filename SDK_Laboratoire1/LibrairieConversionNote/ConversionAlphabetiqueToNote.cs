f0dfc2adb7eec4b7b0139c411ea5ccd753f764b3
itsDiiMS <diegomullersalas@icloud.com>
2023-06-02 15:45:20 -0600

    Jai cree la version du projet visual studio qui transforme les notes comme demande pour le tp1. Tout est fonctionel et pret a remettre.

---
+++ SDK_Laboratoire1/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+
+namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+     
+        public static string Transformer(string lettre)
+        {
+
+
+            if (lettre == "A")
+            {
+                return "La note est LA";
+            }
+            else if (lettre == "B")
+            {
+                return "La note est SI";
+            }
+            else if (lettre == "C")
+            {
+                return "La note est DO";
+            }
+            else if (lettre == "D")
+            {
+                return "La note est RE";
+            }
+            else if (lettre == "E")
+            {
+                return "La note est MI";
+            }
+            else if (lettre == "F")
+            {
+                return "La note est FA";
+            }
+            else if (lettre == "G")
+            {
+                return "La note est SOL";
+            }
+            else if (lettre != "A" || lettre != "B" || lettre != "C" || lettre != "D" || lettre != "E" || lettre != "F" || lettre != "G") {
+                return "La lettre entree est invalide. Veuillez ressayer.";
+            }
+            return lettre;
+        }
+    }
+
+    
+}
--- SDK_Laboratoire1/SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/SDK_Laboratoire1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using LibrairieConversionNote;
+string transformation;
+
+Console.WriteLine("Bienvenue au transformeur de lettres en notes. Veuillez remarquer que les lettres doivent etres en majuscules et que vous avez le choix de A a G. veuillez choisir votre lettre: ");
+do
+{
+    string lettreChoisie = Console.ReadLine();
+    transformation = ConversionAlphabetiqueToNote.Transformer(lettreChoisie);
+    while(transformation == "La lettre entree est invalide. Veuillez ressayer.")
+    {
+        Console.WriteLine("La lettre entree est invalide. Veuillez choisir a nouveau de A a G.");
+        break;
+    }
+} while (transformation == "La lettre entree est invalide. Veuillez ressayer.");
+Console.WriteLine(transformation);
+    
+
+
